d78695f343cc705ee9e9a5eccbbeb2bd640074b1
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-14 10:58:37 +0200

    docs: added comment to createRouter

--- server/createRouter.ts
+++ server/createRouter.ts
@@ -2,5 +2,7 @@ import { router } from "@trpc/server";
 import { Context } from './createContext';
 
 export function createRouter() {
+  // adding config that applies across all routes
+  // for instance, adding superjson
   return router<Context>();
 }
